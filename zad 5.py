e888ec93d4a83b86626b30aad042f0611a24465b
mictom01 <51085317+mictom01@users.noreply.github.com>
2019-05-27 22:31:27 +0200

    Add files via upload

---
+++ zad 5.py
@@ -0,0 +1,38 @@
+class Osoba:
+    def __init__(self, imie, nazwisko, wiek):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.wiek = wiek
+
+    def przedstaw_sie(self):
+        print(f"Jestem {self.imie} {self.nazwisko}. Mam {self.wiek} lat.")
+
+    def urodziny(self):
+        wiek_przed = self.wiek
+        self.wiek += 1
+        return wiek_przed
+
+
+def main():
+
+    Jan = Osoba("Jan", "Nowak", 48)
+    Adam = Osoba("Adam", "Mickiewicz", 220)
+
+
+    Jan.przedstaw_sie()
+    Adam.przedstaw_sie()
+
+    wiek_Adama_przed = Adam.urodziny()
+    Adam.przedstaw_sie()
+    print(f"Wiek Adama sprzed urodzin: {wiek_Adama_przed}")
+
+
+    Jan.imie = "Stanisław"
+    Jan.nazwisko = "Witkiewicz"
+    Jan.wiek = 133
+
+    Jan.przedstaw_sie()
+
+
+if __name__ == "__main__":
+    main()
