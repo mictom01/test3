7b279ed47168f3c944208c11dc2c93749c44c5d4
mictom01 <51085317+mictom01@users.noreply.github.com>
2019-05-27 22:31:00 +0200

    Add files via upload

---
+++ zad 4.py
@@ -0,0 +1,19 @@
+
+class Calculator():
+
+    def dodaj(self, a, b):
+        wynik = a+b
+        print(wynik)
+    def odejmij(self,a,b):
+        wynik = a-b
+        print(wynik)
+
+calc = Calculator()
+
+calc.dodaj(10,15)
+calc.odejmij(5,2)
+
+
+
+
+
